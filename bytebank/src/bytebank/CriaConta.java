e0cd7e1ba55e22ecd4ba77a0ff6daa309d6152e1
Lucas Andrade <lucas.andrade@sbmtechnology.com.br>
2024-07-01 11:56:32 -0300

    Valores default de atributos

--- bytebank/src/bytebank/CriaConta.java
+++ bytebank/src/bytebank/CriaConta.java
@@ -12,5 +12,14 @@ public class CriaConta {
 		
 		System.out.println("primeira conta tem: " + primeiraConta.saldo);
 		System.out.println("segunda conta tem: " + segundaConta.saldo);
+	
+		System.out.println(primeiraConta.agencia);
+		System.out.println(primeiraConta.numero);
+		
+		System.out.println(segundaConta.agencia);
+		
+		segundaConta.agencia = 146;
+		System.out.println("agora a segunda conta esta " + "na agencia " + segundaConta.agencia);
+		
 	}
 }
