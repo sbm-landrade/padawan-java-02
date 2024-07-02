9e831005f82376a65a928615464c187b6df296ce
Lucas Andrade <lucas.andrade@sbmtechnology.com.br>
2024-07-01 22:07:26 -0300

    Nosso primeiro método

---
+++ bytebank/src/bytebank/TestaMetodo.java
@@ -0,0 +1,11 @@
+package bytebank;
+
+public class TestaMetodo {
+	public static void main(String[] args) {
+		Conta contaDoLucas = new Conta();
+		contaDoLucas.saldo = 100;
+		contaDoLucas.deposita(50);
+		System.out.println(contaDoLucas.saldo);
+	}
+
+}
